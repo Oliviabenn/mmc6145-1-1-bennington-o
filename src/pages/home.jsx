92951a997c70c3503d1f847e948fcfeea9537fc8
Olivia <oliviabennington@gmail.com>
2023-01-24 20:30:44 -0500

    contact and about pages added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.css'
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.placeholder}>I'm the home page!</h1>
+      <h1 className={styles.placeholder}>Home page</h1>
     </main>
   )
 }
