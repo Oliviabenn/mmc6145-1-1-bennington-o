a10abefc0bcef3071c0c2eacb2f3789559d2cf76
Olivia <oliviabennington@gmail.com>
2023-01-28 18:17:35 -0500

    contact links added

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
 
 
 const Contact = () => {
     return (
-        <div className><h1>Contact</h1></div>
+        <div className="contact">
+            <h1>Contact</h1>
+            <div>
+                <h2>Let's Get In Touch</h2>
+                <p>If you'd like to get in touch with me, you can always reach me by email.</p>
+                <div>
+                    <p></p>
+                    <ul>
+                        <li><Link to="#">Github</Link></li>
+                        <li><Link to="#">LinkedIn</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Contact;
