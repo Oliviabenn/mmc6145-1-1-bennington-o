9bf6bb1f18dc7cd439d2f6e29ae56f2500137368
Olivia <oliviabennington@gmail.com>
2023-01-28 18:00:24 -0500

    home pg projects styled, text overlay

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,22 +4,22 @@ import { useState } from "react"
 
 export default function Home() {
   const [projects, setProjects]= useState([
-    {image: 'images/image_1', title: 'BadPony Shop', desc: 'Shop specializing in custom apparel'}, 
-    {image: 'images/image_2', title: 'Common Ground', desc: 'Wesbite for finding homes'},
-    {image: 'images/image_3', title: 'TRC Mobile layout', desc: 'Mobile page for TRC Creative Group'}
+    {image: 'images/image_1.jpg', title: 'BadPony Shop', desc: 'Shop specializing in custom apparel'}, 
+    {image: 'images/image_2.jpg', title: 'Common Ground', desc: 'Wesbite for finding homes'},
+    {image: 'images/image_3.jpg', title: 'TRC Mobile layout', desc: 'Mobile page for TRC Creative Group'}
   ]);
 
   return (
     <div className="home">
           <h1>Welcome!</h1>
-          {projects.map(project=> (
-               <div className="project">
-               <img src="images/image_1.jpg" alt="image1"/>
-               <div>
-                 <h2>BadPony Shop</h2>
-                 <p>Shop specializing in custom apparel</p>
-               </div>
-           </div>
+          {projects.map((project, index)=> (
+               <div className="project" key={index}>
+                  <img src={project.image} alt="image1"/>
+                  <div>
+                    <h2>{project.title}</h2>
+                    <p>{project.desc}</p>
+                  </div>
+              </div>
           ))        
          }
    </div>
